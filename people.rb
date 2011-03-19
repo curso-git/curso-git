ee77d97d9f10acabd82b10d9e65b98a5392c57d4
Daniel Lopes <danielvlopes@gmail.com>
2011-03-19 11:53:05 -0300

    mudando tudo

---
+++ people.rb
@@ -0,0 +1,19 @@
+class People
+  attr_accessor :name, :last, :age, :sex
+  
+  def intialize(name, last, age, sex)
+    @name, @last, @age, @sex = name, last, age, sex
+  end
+  
+  def diga_ola
+    if (age < 2) 
+      "cuti, cuti"
+    else
+      "Olá #{full_name} você é #{sex}!"
+    end
+  end
+  
+  def full_name
+    "#{name} #{last}"
+  end
+end
