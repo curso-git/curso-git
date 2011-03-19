8014859dacdc2dc99252470aba80d8b1a1395246
Arthur Zapparoli <arthurzap@gmail.com>
2011-03-19 10:36:17 -0300

    Alterando mensagem de Olá para bebês

--- pessoa.rb
+++ pessoa.rb
@@ -8,6 +8,10 @@ class Pessoa
   end
   
   def diga_ola
-    puts "Olá #{@nome} #{@sobrenome}!"
+    if idade < 2
+      puts "cuti, cuti"
+    else
+      puts "Olá #{@nome} #{@sobrenome}!"
+    end
   end
 end
