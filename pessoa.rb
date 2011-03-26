e2d6b10f33a4d78ba866e8479640528b6857b70b
Arthur Zapparoli <arthurzap@gmail.com>
2011-03-26 08:06:33 -0300

    Melhorando a mensagem de texto

--- pessoa.rb
+++ pessoa.rb
@@ -1,15 +1,15 @@
 class Pessoa
-  attr_accessor :nome_completo, :idade, :sexo
+  attr_accessor :nome_completo, :idade, :sexo, :data_nasc
   
-  def intialize(nome_completo, idade, sexo)
-    @nome_completo, @idade, @sexo = nome, sobrenome, idade, sexo
+  def intialize(nome_completo, idade, sexo, data_nasc)
+    @nome_completo, @idade, @sexo, @data_nasc = nome, sobrenome, idade, sexo, data_nasc
   end
   
-  def diga_ola
+  def to_s
     if (idade < 2) 
       "cuti, cuti"
     else
-      "Olá #{nome_completo} você é #{sexo}!"
+      "Eu me chame #{nome_completo}, e sou um(a) #{sexo} com #{idade} anos!"
     end
   end
 end
